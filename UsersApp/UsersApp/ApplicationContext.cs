d98004d0a1fd63c1e21550cc10970a9585b8fbdb
Artem Viatkin <newyorkbox47@gmail.com>
2022-04-23 20:31:00 +0300

    DB SQlite add

---
+++ UsersApp/UsersApp/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace UsersApp
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users{ get; set; }
+
+        public ApplicationContext() : base("DefaultConnection") { }
+    }
+}
